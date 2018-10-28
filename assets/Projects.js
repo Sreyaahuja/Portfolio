a67ae5653e68816079a58a4725c4a1a4f8138a10
Sreya Ahuja <sreya.ahuja@gmail.com>
2018-10-28 12:46:59 +0530

    Inital Commit with Nuxt and Some Projects

---
+++ assets/Projects.js
@@ -0,0 +1,153 @@
+const Projects = [
+    
+    {
+        name: 'Polarity',
+        posterImage: {
+            ratio: '',
+            src: 'polarity_cover.png',
+            desc: 'A healthcare assitant for those diagonosed with Bipolar Disorders'
+        },
+        tags: ['ux', 'iot', 'healthcare', 'ui', 'research']
+    },
+    {
+        name: 'Game Live',
+        posterImage: {
+            ratio: '',
+            src: 'GameLive_cover.png',
+            desc: 'Livestream games from Samsung smartphones'
+        },
+        tags: ['ux','gaming', 'samsung']
+    },
+    {
+        name: 'Bixby',
+        posterImage: {
+            ratio: '',
+            src: 'bixby_cover.png',
+            desc: 'An intelligent assistant by Samsung'
+        },
+        tags: ['ux','iot', 'samsung']
+    },
+    {
+        name: 'Heart Rate Sensor',
+        posterImage: {
+            src: 'Heart_cover.png',
+            desc: 'Your pulse on your fingertips'
+        },
+        tags: ['ux', 'research', 'ui', 'working model', 'iot']
+    },
+    {
+        name: 'Litrit',
+        posterImage: {
+            src: 'Litrit_cover.png'
+            //A social platform democratizing creative literature
+        },
+        tags: ['ux', 'research', 'ui', 'community']
+    },
+    {
+        name: 'The Florist',
+        posterImage: {
+            src: 'florist_cover.png'
+            //A --- flower retailer
+        },
+        tags: ['visual', 'branding', 'collaterals']
+    },
+    {
+        name: 'Indian Buyers',
+        posterImage: {
+            src: 'eStore_cover.png'
+            //A study on Indian smartphone buyers
+        },
+        tags: ['ux', 'research']
+    },
+    {
+        name: 'Prescrip',
+        posterImage: {
+            src: 'prescrip_cover.png'
+            //Prescriptions, medicine reminders and more!
+        },
+        tags: ['ux', 'healthcare', 'ui']
+    },
+    {
+        name: 'Game Launcher and Tools',
+        posterImage: {
+            src: 'GL&T_cover.png'
+            //The ultimate control center for games on your Samsung smartphone
+        },
+        tags: ['ux', 'gaming', 'ui', 'samsung']
+    },
+    {
+        name: 'Zeitgeist',
+        posterImage: {
+            src: 'Zeitgeist_cover.png'
+            //the spirit of the 2010s
+        },
+        tags: ['ux', 'research']
+    },
+    {
+        name: 'Vegan League',
+        posterImage: {
+            src: 'VeganLeague_cover.png'
+            //Be kind to all kinds
+        },
+        tags: ['ui', 'visual', 'branding', 'collaterals']
+    },
+    {
+        name: 'Vital',
+        posterImage: {
+            src: 'Vital_cover.png'
+            //blood sugar procedure solutions
+        },
+        tags: ['ux', 'healthcare', 'ui', 'working model']
+    },
+    {
+        name: 'Tizen Community',
+        posterImage: {
+            src: 'TizenCommunity_cover.png'
+            //A support community for Tizen
+        },
+        tags: ['ux', 'community', 'ui', 'samsung']
+    },
+    {
+        name: 'Gaming Chair',
+        posterImage: {
+            src: 'GamingChair_cover.png'
+            //
+        },
+        tags: ['ux', 'gaming', 'working model']
+    },
+    {
+        name: 'Whittle',
+        posterImage: {
+            src: 'whittle_cover.png'
+            //High end pottery products
+        },
+        tags: ['visual', 'branding', 'collaterals']
+    },
+    {
+        name: 'Make A Way',
+        posterImage: {
+            src: 'heart-cover.png'
+            //A two player strategy board game
+        },
+        tags: ['ux', 'gaming', 'working model']
+    },
+    {
+        name: 'Strategic Gaming',
+        posterImage: {
+            src: 'heart-cover.png'
+            //Evolution of strategy games and human psychology
+        },
+        tags: ['ux', 'gaming', 'research']
+    },
+    {
+        name: 'Doodle',
+        posterImage: {
+            src: 'doodle_cover.png'
+            //A hypothetical stationary brand
+        },
+        tags: ['visual', 'branding', 'collaterals']
+    },
+    
+]
+
+export default Projects
---
+++ pages/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        Portfolio
+      </h1>
+      <h2 class="subtitle">
+        My Projects
+      </h2>
+      <section class="section">
+        <div class="my-projects container tile">
+          <div
+            v-for="project in projects"
+            :key="project.title"
+            class="project card">
+            <div class="card-image">
+              <figure
+                :class="['image', project.posterImage.ratio ? project.posterImage.ratio: 'is-square',]">
+                <img :src="project.posterImage.src">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">{{ project.name }}</p>
+                  <p class="subtitle is-6">
+                    <a
+                      v-for="tag in project.tags"
+                      :key="tag"
+                      href="#">
+                      {{ tag }}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+import Projects from '~/assets/Projects.js'
+
+export default {
+  data () {
+    return {
+      projects: Projects
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.my-projects {
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+.project.card {
+  min-width: 300px;
+  flex-grow: 0;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
