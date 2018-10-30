fcc28b80ea9dee7b5b5d7f106d518b451eea6ff8
Sreya Ahuja <sreya.ahuja@gmail.com>
2018-10-30 10:11:32 +0530

    Added art images and comments

--- assets/Projects.js
+++ assets/Projects.js
@@ -150,6 +150,16 @@ const Projects = {
         },
         tags: ["ux", "iot", "samsung"]
       },
+      {
+        name: "AERX",
+        Description: "An intelligent assistant by Samsung",
+        contentImage: "Bixby.png",
+        posterImage: {
+          ratio: "",
+          src: "bixby_cover.png"
+        },
+        tags: ["ux", "iot", "samsung"]
+      },
     ],
    
       "Branding": [
@@ -215,5 +225,75 @@ const Projects = {
   ]
   
 };
+
+//Just a few entries from my art journal. These contain random thoughts, expressions and metaphors from my daily life.
+//The Onlooker 
+//Explode
+//Laura
+//Reaching Out
+//Divided
+//DeKooning Flowers
+//Conversations
+//Drowning
+//Cat Woman
+//Cherry Blossom
+//Mandala
+//Yawn
+//Cluelessly Amazed
+//Ruins
+//Boxed
+//The woman
+//Dragonflies
+//Veil
+//What's in you head?
+//She
+//Blue 
+//Cosmic Dreams
+//Eye of the tiger
+//Serenity
+//Of Landscapes
+//Cuddles
+//Brutally Fun
+//Why
+//Scream
+//Angles
+//Scream II
+//Wrapped in a daze
+
+
+//Devil's Advocate - You know what it's like when an excess of anything puts you off that thing completely? Well, that's how a possible cupid turned into the devil's advocate. 
+//On Valentine's week, 2017, as I was walking back home after getting supplies from the supermarket, I realized tons of shops that were strewn with anything that was soft, cuddly, red or in any way, symbolic of love. I probably even smiled at the sight of the first two-three shops that came along. Slowly, but definitely, the sight of these overt displays begging people to profess their love started to annoy me.
+//I should make clear now that i am not a hater of this tradition, the idea of celebrating love, or anything on those lines.
+//But on seeing all these shops, somehow, i wanted to "celebrate" Valentine's week a different way - possibly the devil's way.
+//Here are a few illustrations meant for each one of the days of the Valentine's week.
+//Rose Day: Pride: from the grave
+//Propose Day: Greed: When you have no idea you're engaged to the devil himself. 
+//Chocolate Day: Gluttony: When chocolate is all that you are made up of. And all that you can bleed.
+//Teddy Day: Wrath: When you're not particularly keen on teddy bears.
+//Promise Day: Sloth: When the reaper promises to come back for you!
+//Hug Day: Envy: 
+//Kiss Day: Lust: The kiss of death.
+
+
+//Music and lyrics - "Hello, hello hello... Is there anybody in there?"
+//Most weekday mornings were the same. Staying a good 15 kms away from my place of work, I spent a lot of time travelling to and from my workplace. I tried to use this time productively - read a book / listen to a podcast but either motion sickness or drowsiness kicked in, keeping me from the good these had to offer. I then started to listen to music while on the road.
+//It was a similar sleepy morning and my usual shuffle playlist, when my mind went on to wander, dreamily.
+//"Hello, hello hello... Is there anybody in there?"
+//The reverberation of the hello made me imagine how it was being received by my sensory organs. Being a lover of Biology, I knew my subject relatively well. I could imagine the word travel from my outer ear, through the ear canal, causing vibrations in the eardrum that were finally being amplified and sent to the cochlea - the snail shaped structure filled with fluid.
+//And suddenly, there it was. Stories and images being built and shattered, over and over again, with the harmonic cadence. I mentally took notes of the images and the stories to create them once I was home
+//I don't know what i was more fascinated about- the music, the science, or the way i imagined it to happen.
+//Like any other piece of art, this was far from accurate - it was a representation of a thought, of a feeling that I got while listening to the song.
+//Comfortably Numb, Pink Floyd
+//Fix you, Coldplay
+//Mockingbird, Eminem
+//Nadaan Parinde + Aur Ho, Rockstar
+//Payphone, Maroon 5
+//Cleaning out my closet, Eminem
+//Gravity, John Mayer
+//Sail, Awolnation
+//Imagine, John Lennon
+//Ass like that, Eminem
+//Lost Stars, Kiera Knightley
+
 
 export default Projects;
